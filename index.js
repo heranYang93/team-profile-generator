b70d80c2cb641ed7e167347c5afc7da6cf1b07d4
hy <yang.hrn@gmail.com>
2022-01-16 19:34:18 +0000

    template updated, tailhtml, headhtml,employeeClass

--- index.js
+++ index.js
@@ -2,24 +2,65 @@
 const fs = require("fs"); // file system
 const inq = require("inquirer"); // inquirer
 const que = require("./lib/questions"); //An array of questions for prompt lines
-const emp = require("./lib/employee"); //Make readme template
+const Template = require("./lib/template");
+const headHTML = require("./lib/headHTML");
+const tailHTML = require("./lib/tailHTML");
 
-console.log(que.questionsIntern);
-console.log(emp);
+async function init() {
+  let htmlContent = headHTML;
 
-// async function init() {
-//   // Get user data
-//   let userData = "";
-//   await i.prompt(questionArr).then((r) => (userData = r));
-//   let templateObj = new Templify(userData);
-//   templateObj.tableOfContentGen();
-//   templateObj.licenseGen(licenseArr);
-//   templateObj.contentGen();
-//   fs.writeFile("./Generated/README.md", templateObj.allIn, (e, r) => {
-//     e ? console.error("Error") : console.log("Done");
-//   });
-// }
+  let thisManagerData;
+  await inq.prompt(que.questionsManager).then((r) => (thisManagerData = r));
+  managerTemp = new Template(
+    thisManagerData.managerName,
+    "Manager",
+    thisManagerData.managerId,
+    thisManagerData.managerEmail,
+    "Office Number",
+    thisManagerData.managerOffice
+  );
+  htmlContent += managerTemp.make();
 
-function init() {}
+  let addition;
+  await inq.prompt(que.questionsAdd).then((r) => (addition = r));
+
+  while (addition.createNew !== "Finish") {
+    if (addition.createNew === "Add Engineer") {
+      let thisEngData;
+      await inq.prompt(que.questionsEngineer).then((r) => (thisEngData = r));
+      engineerTemp = new Template(
+        thisEngData.engName,
+        "Engineer",
+        thisEngData.engId,
+        thisEngData.engEmail,
+        "Github",
+        thisEngData.engGithub
+      );
+      htmlContent += engineerTemp.make();
+      //add an HTML section for this engineer
+      await inq.prompt(que.questionsAdd).then((r) => (addition = r));
+    } else {
+      let thisIntData;
+      await inq.prompt(que.questionsIntern).then((r) => (thisIntData = r));
+      internTemp = new Template(
+        thisIntData.intName,
+        "Intern",
+        thisIntData.intId,
+        thisIntData.intEmail,
+        "School",
+        thisIntData.intSchool
+      );
+      htmlContent += internTemp.make();
+
+      await inq.prompt(que.questionsAdd).then((r) => (addition = r));
+    }
+  }
+
+  htmlContent += tailHTML;
+
+  fs.writeFile("./dist/pages/index.html", htmlContent, (e, r) => {
+    e ? console.error("Error") : console.log("Team Page Created Successfully");
+  });
+}
 
 init(); //Run app
